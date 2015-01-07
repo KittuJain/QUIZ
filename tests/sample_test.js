b6ca9f4f6774bfe5f8eb261e5f31e62258a1ca6b
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-01-07 15:10:43 +0530

    sample test for ci server

---
+++ tests/sample_test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+describe('#add', function() {
+	it('2+3=5', function(done) {
+		var add = function() {
+			return 2 + 3;
+		}
+		assert.equal(5, add());
+		done();
+	});
+});
