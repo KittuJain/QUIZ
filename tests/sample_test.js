1b29ecfcaacc3f729809a4b8d2f3c4fb04f52698
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-01-07 15:25:44 +0530

    commented the dummy failing test for ciS

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -10,12 +10,12 @@ describe('#add', function() {
 	});
 });
 
-describe('#addWrong', function() {
-	it('2+3=5', function(done) {
-		var add = function() {
-			return 2 + 3;
-		}
-		assert.equal(6, add());
-		done();
-	});
-});
+// describe('#addWrong', function() {
+// 	it('2+3=5', function(done) {
+// 		var add = function() {
+// 			return 2 + 3;
+// 		}
+// 		assert.equal(6, add());
+// 		done();
+// 	});
+// });
