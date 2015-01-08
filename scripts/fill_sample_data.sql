1479aa7bd1c442b4e33a512c54ceb00256d26bc3
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-01-08 18:17:42 +0530

    A3S06:Ananthu&Vijay:created test setup and lib setup

--- scripts/fill_sample_data.sql
+++ scripts/fill_sample_data.sql
@@ -1,1 +1,3 @@
 PRAGMA foreign_keys = "ON";
+INSERT INTO topics (name,duration,useremail) values ('GK','00:30:00','abc@email.com'),
+('SS','00:20:00','efg@email.com'),('Language','00:15:00','efg@email.com');
