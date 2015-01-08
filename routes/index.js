c39fc5b4c875fe3a5ed0594b578c8b5a0dbec10c
parmatma <pyadav.it@gmail.com>
2015-01-08 19:54:15 +0530

    A1S01 : Krati Parmatma : User can register

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var lib = require('../lib/quiz.js');
+var quiz = lib.init('data/quiz.db');
 var router = express.Router();
+var bcrypt = require("bcryptjs");
 
 router.get('/', function(req, res) {
   res.render('index');
@@ -8,5 +11,23 @@ router.get('/', function(req, res) {
 router.get('/login',function(req,res){
 	res.render('login');
 });
+
+router.post('/login',function(req,res,next){
+	if(req.body.exist){
+		loginUser(req,res,next);
+	}else{
+		registerUser(req,res,next);
+	}
+});
+
+var registerUser = function(req,res,next){
+	var password  = bcrypt.hashSync(req.body.password);
+	var new_user = {"email":req.body.email,"password":password};
+	quiz.addUser(new_user,function(err,user){
+		req.session.userEmail = user.email;
+		if(!err)
+			res.render('dashboard',{user:user});		
+	});
+};
 
 module.exports = router;
