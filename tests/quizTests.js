43ccee6c85aefa7bd40712b3f7f9705fcb62bd67
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-01-08 18:45:35 +0530

    A3S06:Ananthu&Vijay:added the get topics function to retrive the topics from the topics table in quiz db

--- tests/quizTests.js
+++ tests/quizTests.js
@@ -9,5 +9,25 @@ describe('quiz', function() {
 		fs.writeFileSync('tests/data/quiz.db', dbFileData);
 		quiz = lib.init('tests/data/quiz.db');
 	});
-	
+
+	describe('#getTopics', function() {
+		it('retrives the all the topic ids,names other than the owners', function(done) {
+			var userEmail = 'abc@email.com';
+			var expected = [{
+				id: 2,
+				name: 'SS',
+				duration: '00:20:00'
+			}, {
+				id: 3,
+				name: 'Language',
+				duration: '00:15:00'
+			}];
+			quiz.getTopics(userEmail, function(err, topics) {
+				assert.notOk(err);
+				assert.deepEqual(topics, expected);
+				done();
+			});
+		});
+	});
+
 });
