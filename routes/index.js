d329265c941fb961220129ef08dba129347879d0
unknown <vijayps@INVIJAYPS.corporate.thoughtworks.com>
2015-01-09 14:58:58 +0530

    A3S06:Ananthu&Vijay:displayed error message if no quizzes are found is done on the quizzes jade

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var lib = require('../lib/quiz.js');
+var quiz = lib.init('data/quiz.db');
 
 router.get('/', function(req, res) {
-  res.render('index');
+	res.render('index');
 });
 
-router.get('/login',function(req,res){
+router.get('/login', function(req, res) {
 	res.render('login');
 });
+
+router.get('/quizzes', function(req, res) {
+	console.log(quiz)
+	quiz.getTopics('abc@email.com', function(err, topics) {
+		var quizzes = {};
+		topics.length == 0 ? quizzes.quizzes = false : quizzes.quizzes = topics;
+		res.render('quizzes', quizzes);
+	})
+})
 
 module.exports = router;
