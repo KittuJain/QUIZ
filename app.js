3f2b25f4c275644fd311560213a4d46749438258
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-06-16 00:42:06 +0530

    Kaustav - [housekeeping] - Updated UX for Home, Login and Dashboard. New UI files added.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret:"secret hai bhai"}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
