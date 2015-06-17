cb302a3e72ad6e581751bb5c8ee539a826505f8f
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-06-17 21:14:39 +0530

    Kaustav - #housekeeping - Refactored index.js in routes. Updated SQlite3 version in package.json. created new router called users for user interactions.

--- routes/index.js
+++ routes/index.js
@@ -4,99 +4,51 @@ var quiz = lib.init('data/quiz.db');
 var getDb = require('../lib/DBLib.js').create_db('data/quiz.db');
 var gameLib = require('../lib/game.js').lib;
 var router = express.Router();
-var bcrypt = require("bcryptjs");
+var users = require("./users").users;
+//var display = require("./display").display;
 
-router.get('/', function(req, res) {
-	res.render('index');
+router.get('/', function(request, response) {
+	response.render('index');
 });
 
-var requireLogin = function(req,res,next){
-	req.session.userEmail ? next(): res.redirect('/login');
-};
-
-router.get('/login', function(req, res) {
-	res.render('login');
+router.get('/login', function(request, response) {
+	users.get.login(request, response);
 });
 
-router.get('/quizzes',requireLogin, function(req, res) {
-	console.log(req.session.userEmail)
-	quiz.getTopics(req.session.userEmail, function(err, topics) {
-		var quizzes = {email:req.session.userEmail};
-		topics.length == 0 ? quizzes.quizzes = false : quizzes.quizzes = topics;
-		res.render('quizzes', quizzes);
-	});
+router.get('/quizzes', function(request, response) {
+    quiz.getTopics(request.session.userEmail, function(err, topics) {
+        var quizzes = {email:request.session.userEmail};
+        topics.length == 0 ? quizzes.quizzes = false : quizzes.quizzes = topics;
+        response.render('quizzes', quizzes);
+    });
 });
 
-router.post('/login', function(req, res, next) {
-	if (req.body.exist) {
-		loginUser(req, res, next);
-	} else {
-		quiz.getUser(req.body.email,function(err,ext){
-			if(ext)
-				res.render('login',{error:"User Email ID already exists"});
-			else
-				registerUser(req, res, next);
-		});
-	}
+router.post('/login', function(request, response) {
+    users.post.login(request, response);
 });
 
-var registerUser = function(req, res, next) {
-	var password = bcrypt.hashSync(req.body.password);
-	var new_user = {
-		"email": req.body.email,
-		"password": password
-	};
-	quiz.addUser(new_user, function(err, user) {
-
-		if (!err){ 
-			req.session.userEmail = user.useremail;
-			res.redirect('/dashboard');
-		}else{
-			res.render('login',{error:"Server Error..."});
-		}
-	});
-};
-
-var loginUser = function(req,res,next){
-	var user = {"email":req.body.email,"password":req.body.password};
-	quiz.getUser(user.email,function(err,existingUser){
-		if(!existingUser){
-			res.render('login',{error:'Incorrect E-mail Id or password'});
-		}
-		else{
-			var isValidPassword = bcrypt.compareSync(user.password,existingUser.password);
-			if(isValidPassword){
-				req.session.userEmail = user.email;
-				res.redirect('/dashboard') 	
-			}
-			else
-				res.render('login',{error:'Incorrect E-mail Id or password'});
-		}
-	});
-};
-
-router.get('/dashboard', function(req, res) {
-	if(req.session.userEmail)
-		res.render('dashboard',{email:req.session.userEmail});
+router.get('/dashboard', function(request, response) {
+	if(request.session.userEmail)
+		response.render('dashboard',{email:request.session.userEmail});
 	else
-		res.redirect('login');
+		response.redirect('login');
 });
 
-router.get('/join/:id',requireLogin, function(req, res) {
+router.get('/join/:id', function(request, response) {
 	var db = getDb();
-	var quizId = req.params.id;
-	var useremail = req.session.userEmail;
+	var quizId = request.params.id;
+	var useremail = request.session.userEmail;
 	gameLib.join(useremail,quizId,db,function(err){
-		res.redirect("/quiz/"+quizId);
+		response.redirect("/quiz/"+quizId);
 	})
 });
-router.get('/quiz/:id', requireLogin, function(req, res) {
+router.get('/quiz/:id', function(request, response) {
 	var db = getDb();
-	var quizId = req.params.id;
+	var quizId = request.params.id;
 	gameLib.load(quizId,db,function(err,gameData){
 		var game = gameLib.start(gameData);
 		var question1 = game.getQuestion();
-		res.render('quiz',{question:question1.q});
+		response.render('quiz',{question:question1.q});
 	})
 });
 module.exports = router;
